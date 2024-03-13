3aa8643b7431eea51f2a8c32d23203e9c7d3f4d5
take331 <t.takeru0331@icloud.com>
2024-03-13 22:44:03 +0900

    🗃️ クイズデータの作成

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "What is the capital of France?",
+    option1: "London",
+    option2: "Paris",
+    option3: "Rome",
+    option4: "Berlin",
+    ans: 2,
+  },
+  {
+    question: "Which planet is known as the 'Red Planet'?",
+    option1: "Earth",
+    option2: "Mars",
+    option3: "Venus",
+    option4: "Jupiter",
+    ans: 2,
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    option1: "Vincent van Gogh",
+    option2: "Pablo Picasso",
+    option3: "Leonardo da Vinci",
+    option4: "Michelangelo",
+    ans: 3,
+  },
+  {
+    question: "What is the chemical symbol for water?",
+    option1: "H2O",
+    option2: "CO2",
+    option3: "NaCl",
+    option4: "O2",
+    ans: 1,
+  },
+  {
+    question: "Which famous playwright wrote Romeo and Juliet?",
+    option1: "William Shakespeare",
+    option2: "George Bernard Shaw",
+    option3: "Arthur Miller",
+    option4: "Tennessee Williams",
+    ans: 1,
+  },
+]
